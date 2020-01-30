cd6d782bb600bf7d866f080a56488138b1222058
aayushagarwal28 <aayush.agarwal28@gmail.com>
2020-01-30 22:36:22 +0530

    Update sms-handler.js

--- sms-handler.js
+++ sms-handler.js
@@ -1,7 +1,7 @@
 
 
 
-
+const otp_input=document.querySelector('#otp-input');
 async function main() {
   if (!navigator.sms) {
     alert("feature not available :(");
@@ -10,6 +10,15 @@ async function main() {
   try {
     let {content} = await navigator.sms.receive();
     alert("sms received! " + content);
+      let regex = this.getAttribute("regex");
+            let code = new RegExp(regex).exec(content);
+            if (!code) {
+                console.log("SMS message doesn't match regex");
+                 return;
+            }
+       
+   
+    otp_input.value = code;
   } catch (e) {
     alert("time out!");
   }
